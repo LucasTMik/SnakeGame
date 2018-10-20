548ebe1a1297af783214abc3073149a763ce39fc
Lucas <lucascercun@gmail.com>
2018-10-20 20:45:19 -0300

    second commit

--- controler.js
+++ controler.js
@@ -4,6 +4,7 @@ var count = 0;
 
 var foodPos;
 
+//Botão de inicio
 startBtn.onclick = () => {
     setInterval(() => {
         startBtn.disabled = true;
@@ -11,19 +12,20 @@ startBtn.onclick = () => {
         moveSnake();  
     } ,100);
     count++;
+    food();
 }
 
+//Botão para pause
 stopBtn.onclick = () => {
     startBtn.disabled = false;
     startBtn.style = "background-color: black";
     clearInterval(count);
 };
 
-
+//Função que gera a comida em uma célula aleatória
 let food = () => {
     foodPos = Math.floor(Math.random()*2399);
     cels[foodPos].style = "background-color: white";
 }
 
 
-food();
